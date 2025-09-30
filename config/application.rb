7faccb5adab72eeebd91fd4b3449d20948e72dab
Martin Lindgren <marlin002@gmail.com>
2025-09-30 22:42:01 +0200

    Configure Active Job to use GoodJob

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Nina
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Use GoodJob for background jobs
+    config.active_job.queue_adapter = :good_job
   end
 end
