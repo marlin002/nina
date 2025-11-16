2d20ec62840fa821527614c4001821b6871791d4
Martin Lindgren <marlin002@gmail.com>
2025-11-16 18:44:09 +0100

    Allow more browswer versions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # Allow browsers from the last few years
+  # This supports a wider range of browsers while excluding very old versions
+  # See: https://github.com/basecamp/browsers for version options
+  # allow_browser versions: :modern  # Too restrictive - only latest browsers
+  # Comment out to allow all browsers:
+  # allow_browser versions: :modern
 end
