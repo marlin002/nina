18f750a2519ea0a5872d6fa52dbcb57e87587e24
Martin Lindgren <marlin002@gmail.com>
2025-12-20 22:24:38 +0100

    Added search route.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # Top-level HTML search (simple query via ?q=...)
+  get "search", to: "search#index", as: :search
+
   # API routes
   namespace :api do
     namespace :v1 do
