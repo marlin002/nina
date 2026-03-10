ac1c707e23c8c5e174e3ebd2cf501332d9ad82e6
Martin Lindgren <marlin002@gmail.com>
2026-03-10 21:16:44 +0100

    New raw layout.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-require 'redis'
+require "redis"
 # Detta tvingar Rails att använda miljövariabeln om den finns, annars localhost
 redis_url = ENV.fetch("REDIS_URL", "redis://192.168.2.15:6379/0")
 REDIS = Redis.new(url: redis_url)
